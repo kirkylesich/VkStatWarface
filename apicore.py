6dafcffc0e54722939652388535ebbb50c581ed8
kirill <stealer-mail@mail.ru>
2020-01-22 00:14:18 +0500

    Added api

--- apicore.py
+++ apicore.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import re
 from bs4 import BeautifulSoup
 
 
@@ -26,6 +27,24 @@ class WarfacePageUser:
             return tag.previous_sibling.text 
         return tag.next_sibling.text
 
+class WarfaceUserInfoForClass:
+    class_name = 'Штурмовик'
+
+    def __init__(self, soup):
+        self.soup = soup
+    
+    def get_all_blocks_of_class(self):
+        return self.soup.find_all('div', class_='statistics-block')
+    
+    def get_current_class_spans(self):
+        class_block = self.soup.find('div', text=re.compile(f'^{self.class_name}'))
+        return class_block.find_all_next('span')[:12]
+
+    def get_value_by_name_field(self, field_name):
+        class_fields = self.get_current_class_spans()
+        for value in class_fields:
+            if value.text == field_name:
+                return class_fields[class_fields.index(value)+1].text
 
 class WarfaceUserProfile(WarfacePageUser):
     profile_config = 'profile'
@@ -78,8 +97,7 @@ class WarfaceUserProfile(WarfacePageUser):
     def restored_ob_value(self):
         return self.get_skill_value_by_skill_name('Восстановлено ОБ')
 
-class WarfaceUserPvp(WarfacePageUser):
-    profile_config = 'pvp'
+class WarfaceUserMainInfo(WarfacePageUser):
 
     @property
     def all_kills(self):
@@ -133,7 +151,92 @@ class WarfaceUserPvp(WarfacePageUser):
     def winrate(self):
         return self.get_value_by_name_field('Процент побед')
 
-    
+    @property
+    def count_kicks_from_match(self):
+        return self.get_value_by_name_field('Исключений из боя')
 
-wf_user = WarfaceUserPvp('Пираний')
-print(wf_user.winrate)
+    @property
+    def count_interrupted_mathces(self):
+        return self.get_value_by_name_field('Прервано матчей')
+    
+    @property
+    def game_time(self):
+        return self.get_value_by_name_field('Время в игре')
+    
+    @property
+    def engineer(self):
+        engineer = WarfaceEngineerClass(self.soup)
+        return engineer
+    
+    @property
+    def medic(self):
+        medic = WarfaceMedicClass(self.soup)
+        return medic
+    
+    @property
+    def rifleman(self):
+        rifleman = WarfaceRiflemanClass(self.soup)
+        return rifleman
+
+    @property
+    def sniper(self):
+        sniper = WarfaceSniperClass(self.soup)
+        return sniper
+    
+    @property
+    def sad(self):
+        sad = WarfaceSadClass(self.soup)
+        return sad
+
+class WarfaceUserClassInterface(WarfaceUserInfoForClass):
+    class_name = 'Штурмовик'
+
+    @property
+    def all_shoots(self):
+        return self.get_value_by_name_field('Произведено выстрелов')
+    
+    @property
+    def hits_in_target(self):
+        return self.get_value_by_name_field('Попаданий в цель')
+    
+    @property
+    def hit_accuracy(self):
+        return self.get_value_by_name_field('Меткость')
+    
+    @property
+    def hits_in_head(self):
+        return self.get_value_by_name_field('Попаданий в голову')
+    
+    @property
+    def hits_in_head_with_knife(self):
+        return self.get_value_by_name_field('Попаданий в голову в ближ. бою')
+    
+    @property
+    def game_time(self):
+        return self.get_value_by_name_field('В игре')
+
+class WarfaceRiflemanClass(WarfaceUserClassInterface):
+    class_name = 'Штурмовик'
+
+class WarfaceMedicClass(WarfaceUserClassInterface):
+    class_name = 'Медик'
+
+class WarfaceSadClass(WarfaceUserClassInterface):
+    class_name = 'СЭД'
+
+class WarfaceEngineerClass(WarfaceUserClassInterface):
+    class_name = 'Инженер'
+
+class WarfaceSniperClass(WarfaceUserClassInterface):
+    class_name = 'Снайпер'
+        
+class WarfaceUserMainPvp(WarfaceUserMainInfo):
+    profile_config = 'pvp'
+    
+class WarfaceUserMainPve(WarfaceUserMainInfo):
+    profile_config = 'pve'
+
+
+
+wf_user = WarfaceUserMainPvp('Пираний')
+print(wf_user.sad.game_time)
